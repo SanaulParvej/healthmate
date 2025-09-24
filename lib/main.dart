1817739a23b0ee501ec85d66433cebc2901a8dc7
SanaulParvej <perves15-6433@s.diu.edu.bd>
2025-09-25 00:39:04 +0600

    implement app icon and fixed some bug

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class HealthMateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HealthMate',
+      title: 'Health Mate',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
